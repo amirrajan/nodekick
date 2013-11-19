c4d92133bb69019959cfdc29528eb4b2ce55fa3c
Amir Rajan <ar@amirrajan.net>
2013-11-19 11:49:02 -0600

    added death fadeout

--- public/app/drawer.js
+++ public/app/drawer.js
@@ -1,14 +1,17 @@
 (function() {
   var game = null;
   var sprites = null;
-  var c, canvas = null;
+  var c, canvas, deathCanvas, deathContext = null;
   var stageHeight = 500;
+  var maxDeathCountdown = 60;
 
   function init() {
     game = app.game;
     sprites = app.assets.sprites;
     canvas = window.document.getElementById('stage');
     c = canvas.getContext('2d');
+    deathCanvas = window.document.getElementById('deathCanvas');
+    deathContext = deathCanvas.getContext('2d');
   }
 
   function draw() {
@@ -20,13 +23,18 @@
     _.each(game.players(), function(player) {
       //drawHitBox(player);
       var sprite = spriteFor(player);
-      c.drawImage(sprite.image, sprite.x, sprite.y);
+      if(sprite.image.toString() == "[object ImageData]") {
+        c.putImageData(sprite.image, sprite.x, sprite.y);
+      }
+      else {
+        c.drawImage(sprite.image, sprite.x, sprite.y);
+      }
     });
   }
 
   function drawHitBox(player) {
     c.fillRect(player.x, player.y + stageHeight, 2, 2); 
-    _.each(game.boxes()[player.direction][player.state], function(box) {
+    _.each(game.boxes()[player.direction][playerState(player)], function(box) {
       drawBox(player, box);
     });
   }
@@ -39,14 +47,56 @@
       Math.abs(box.y2 - box.y));
   }
 
+  function imageDataForDeath(player, sprite, canvasX, canvasY) {
+    deathContext.clearRect(0, 0, 75, 150);
+    deathContext.drawImage(sprite, 0, 0, 75, 150, 0, 0, 75, 150);
+    var stageSource = c.getImageData(canvasX, canvasY, 75, 150);
+    var deathImage = deathContext.getImageData(0, 0, 75, 150);
+    var length = deathImage.data.length;
+    var alphaPercentage = player.deathCountdown / maxDeathCountdown;
+
+    //make white and add fade out
+    for(var i = 3; i < length; i += 4) {
+      if(deathImage.data[i] > 0 && deathImage.data[i - 1] > 0 && deathImage.data[i - 2] > 0 && deathImage.data[i - 3] > 0) {
+        deathImage.data[i-1] = deathImage.data[i-1] + 150;
+        deathImage.data[i-2] = deathImage.data[i-2] + 150;
+        deathImage.data[i-3] = deathImage.data[i-3] + 150;
+        deathImage.data[i] *= alphaPercentage;
+      }
+    }
+
+    //mix with image from the stage 
+    deathContext.putImageData(stageSource, 0, 0);
+
+    var mixedWithStage = deathContext.getImageData(0, 0, 75, 150);
+    for(var i = 3; i < length; i += 4) {
+      if(deathImage.data[i] > 0 && deathImage.data[i - 1] > 0 && deathImage.data[i - 2] > 0 && deathImage.data[i - 3] > 0) {
+        mixedWithStage.data[i-1] = deathImage.data[i-1];
+        mixedWithStage.data[i-2] = deathImage.data[i-2];
+        mixedWithStage.data[i-3] = deathImage.data[i-3];
+        mixedWithStage.data[i] = deathImage.data[i];
+      }
+    }
+
+    return mixedWithStage;
+  }
+
+  function playerState(player) {
+    if(player.state == "dying") return player.deathState;
+
+    return player.state;
+  }
+
   function spriteFor(player) {
-    var state = player.state;
-    if(state == "dying") state = player.deathState;
-    return {
-      image: sprites.dive[player.direction][state],
-      x: player.x - game.boxes().playerCenter,
-      y: (player.y + stageHeight) - game.boxes().playerHeight
-    };
+    var state = playerState(player);
+    var x = player.x - game.boxes().playerCenter;
+    var y = (player.y + stageHeight) - game.boxes().playerHeight;
+    var image = sprites.dive[player.direction][state];
+    if(player.state == "dying") {
+      image = imageDataForDeath(player, image, x, y);
+    }
+
+    return { image: image, x: x, y: y };
   }
 
   app.drawer = { };
