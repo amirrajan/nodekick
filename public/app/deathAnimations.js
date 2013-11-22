b7942fffea43a574b5fd3d8d78a82d6436647444
Amir Rajan <ar@amirrajan.net>
2013-11-21 18:13:30 -0600

    added shatter effect

--- public/app/deathAnimations.js
+++ public/app/deathAnimations.js
@@ -1,35 +1,76 @@
 (function() {
   var deathQueue = [ ];
-  var maxDeathCountdown = 40.0;
+  var maxDeathCountdown = 60.0;
   var stage;
+  var gravity = 1;
+
 
   function init() {
     stage = app.drawer.stage();
   }
 
+  function pieces(texture, x, y) {
+    var results = [ ];
+    var width = texture.frame.width;
+    var height = texture.frame.height;
+    var rows = 20;
+    var columns = 20;
+    var deltaX = width / columns;
+    var deltaY = height / rows;
+    _.times(rows, function(row) {
+      _.times(columns, function(column) {
+        var spriteX = column * deltaX;
+        var spriteY = row * deltaY;
+        var sprite = new PIXI.Sprite(new PIXI.Texture(texture, { x: spriteX, y: spriteY, width: deltaX, height: deltaY }));
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 1;
+        sprite.position.x = x + (width * sprite.anchor.x) - (deltaX * (columns - (column + 1)));
+        sprite.position.y = y - (deltaY * (rows - (row + 1)));
+        results.push({
+          sprite: sprite,
+          dy: _.random(-20, 0),
+          dx: _.random(-20, 20),
+          rotation: Math.random()
+        });
+      });
+    });
+
+    return results;
+  }
+
   function queue(texture, x, y) {
-    var sprite = new PIXI.Sprite(texture); 
-    sprite.anchor.x = 0.5;
-    sprite.anchor.y = 1;
-    sprite.position.x = x;
-    sprite.position.y = y;
+    var bodyParts = pieces(texture, x, y);
 
     var animation = {
-      sprite: sprite,
+      pieces: bodyParts,
       deathCountdown: maxDeathCountdown
     };
 
     deathQueue.push(animation); 
-    stage.addChild(animation.sprite);
+    _.each(animation.pieces, function(piece) { stage.addChild(piece.sprite); });
   }
 
   function draw() {
     _.each(deathQueue, function(death) {
-      death.sprite.alpha = death.deathCountdown / maxDeathCountdown;
+      _.each(death.pieces, function(piece) {
+        piece.sprite.alpha = death.deathCountdown / maxDeathCountdown;
+        piece.sprite.position.x += piece.dx;
+        piece.sprite.position.y += piece.dy;
+        piece.dy += gravity;
+        piece.sprite.rotation += piece.rotation;
+      });
       death.deathCountdown -= 1;
     });
 
-    deathQueue = _.without(deathQueue, _.where(deathQueue, { deathCountdown: 0 }));
+    var done = _.where(deathQueue, { deathCountdown: 0 });
+
+    _.each(done, function(d) {
+      _.each(d.pieces, function(piece) {
+        stage.removeChild(piece.sprite);
+      });
+    });
+
+    deathQueue = _.without(deathQueue, done);
   }
 
   app.deathAnimations = { };
