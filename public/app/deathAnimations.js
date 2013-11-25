700a2907f0191217222ebcb190a557e39b0f6eef
Amir Rajan <ar@amirrajan.net>
2013-11-24 21:48:53 -0600

    added death animation explosion in the z direction

--- public/app/deathAnimations.js
+++ public/app/deathAnimations.js
@@ -1,6 +1,6 @@
 (function() {
   var deathQueue = [ ];
-  var maxDeathCountdown = 150;
+  var maxDeathCountdown = 100;
   var stage;
   var gravity = 0.2;
 
@@ -13,8 +13,8 @@
     var results = [ ];
     var width = texture.frame.width;
     var height = texture.frame.height;
-    var rows = 15;
-    var columns = 15;
+    var rows = 20;
+    var columns = 10;
     var deltaX = width / columns;
     var deltaY = height / rows;
     _.times(rows, function(row) {
@@ -23,13 +23,15 @@
         var spriteY = row * deltaY;
         var sprite = new PIXI.Sprite(new PIXI.Texture(texture, { x: spriteX, y: spriteY, width: deltaX, height: deltaY }));
         sprite.anchor.x = 0.5;
-        sprite.anchor.y = 1;
+        sprite.anchor.y = 0.5;
         sprite.position.x = x + (width * sprite.anchor.x) - (deltaX * (columns - (column + 1)));
         sprite.position.y = y - (deltaY * (rows - (row + 1)));
         results.push({
           sprite: sprite,
           dy: _.random(-20, 0),
           dx: _.random(-10, 10),
+          scale: Math.random() * 0.05,
+          scaleDirection: Math.random() > 0.5 ? -1 : 1,
           rotation: Math.random() / 2
         });
       });
@@ -56,17 +58,21 @@
         piece.sprite.alpha = death.deathCountdown / maxDeathCountdown;
         var dx = piece.dx;
         var dy = piece.dy;
+        var scale = piece.scale * piece.scaleDirection;
         var rotation = piece.rotation;
         var tickCount = maxDeathCountdown - death.deathCountdown;
 
         if(tickCount > 5 && tickCount < 25) {
-          dx = dx / 10;
+          dx = (dx / 10);
           dy = dy / 10;
           rotation = rotation / 10;
+          scale = scale / 10;
         }
 
         piece.sprite.position.x += dx;
         piece.sprite.position.y += dy;
+        piece.sprite.scale.x += scale;
+        piece.sprite.scale.y += scale;
         piece.dy += gravity;
         piece.sprite.rotation += rotation;
       });
