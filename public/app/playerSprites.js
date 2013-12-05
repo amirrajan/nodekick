2a40fb686cb8a9b640a87942b82c1c7970fbf675
Amir Rajan <ar@amirrajan.net>
2013-12-05 16:34:01 -0600

    added names to players

--- public/app/playerSprites.js
+++ public/app/playerSprites.js
@@ -29,6 +29,7 @@
     var playerSprite = new PlayerSprite(player.id);
     sprites[player.id] = playerSprite;
     stage.addChild(playerSprite.sprite);
+    stage.addChild(playerSprite.name);
   }
 
   function tick(players) {
@@ -39,18 +40,35 @@
   function PlayerSprite(playerId) {
     this.id = playerId;
     this.sprite = new PIXI.Sprite(playerTexture(this.id));
+    var base = { font: "10pt Arial" };
+
+    if(app.game.me().id == this.id) {
+      base = {
+        font: "10pt Arial",
+        fill: "black",
+        stroke: "black",
+        strokeThickness: 1
+      };
+    }
+
+    this.name = new PIXI.Text(app.game.getPlayer(this.id).name, base);
     this.sprite.anchor.x = 0.5;
     this.sprite.anchor.y = 1;
+    this.name.anchor.x = 0.5;
+    this.name.anchor.y = 1;
     this.dead = false;
     this.tick = function() {
       var player = app.game.getPlayer(this.id);
       this.sprite.alpha = 0;
+      this.name.alpha = 0;
 
       if(!player) return;
 
       this.sprite.setTexture(playerTexture(this.id));
       this.sprite.position.x = player.x;
       this.sprite.position.y = player.y + stageHeight;
+      this.name.position.x = player.x;
+      this.name.position.y = player.y + stageHeight - 150;
 
       if(player.state == "dying" && !this.dead) { 
         this.dead = true;
@@ -61,6 +79,7 @@
       } else if(player.state != "dying") {
         this.dead = false;
         this.sprite.alpha = 1;
+        this.name.alpha = 1;
       }
     };
   }
