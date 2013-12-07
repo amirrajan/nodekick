0c5a92b5cecb85a91136a20c1492b1fd83e1a678
Amir Rajan <ar@amirrajan.net>
2013-12-07 10:05:25 -0600

    chat moved to seperate module

---
+++ public/app/chat.js
@@ -0,0 +1,20 @@
+(function() {
+  function init() {
+    $(document).keydown(function(e) {
+      if(e.keyCode == 13) {
+        if($("#chatMessage").val() == "") return;
+
+        app.game.sendChat($("#chatMessage").val());
+        $("#chatMessage").val('');
+      }
+    });
+
+    app.game.chatReceived = function(from, message) {
+      $("<div></div>").text(from + ": " + message).prependTo("#chatMessages");
+    };
+  }
+
+  app.chat = { };
+  app.chat.init = init;
+})();
+
--- public/app/index.js
+++ public/app/index.js
@@ -2,6 +2,7 @@
   function init() {
     app.game.init();
     app.achievements.init();
+
     app.gamePad.init({
       up: app.game.up,
       left: app.game.left,
@@ -14,19 +15,8 @@
     app.deathAnimations.init();
     app.playerSprites.init();
     app.ai.init();
+    app.chat.init();
     app.drawer.startDrawing();
-    app.game.chatReceived = function(from, message) {
-      $("<div></div>").text(from + ": " + message).prependTo("#chatMessages");
-    };
-
-    $(document).keydown(function(e) {
-      if(e.keyCode == 13) {
-        if($("#chatMessage").val() == "") return;
-
-        app.game.sendChat($("#chatMessage").val());
-        $("#chatMessage").val('');
-      }
-    });
   }
 
   $(init);
